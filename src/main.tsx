eceeea1be6f1fe41731f9369c92ca3e923890931
lyutails <lyusya.cool@gmail.com>
2023-11-06 16:33:54 +0300

    feat: remove commented code from main

--- src/main.tsx
+++ src/main.tsx
@@ -4,29 +4,6 @@ import './index.module.scss';
 import ErrorBoundary from './error_boundary';
 import { App } from './app';
 
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <ErrorPage></ErrorPage>,
-//     /* loader: */
-//     children: [
-//       {
-//         path: 'species/:id',
-//         element: <CardDetails />,
-//       },
-//     ],
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <ErrorBoundary>
-//       <RouterProvider router={router} />
-//     </ErrorBoundary>
-//   </React.StrictMode>
-// );
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>

668e82524604baad5f122ba6942421b9394d4456
lyutails <lyusya.cool@gmail.com>
2023-11-06 06:44:55 +0300

    feat: unnecessary comments

--- src/main.tsx
+++ src/main.tsx
@@ -6,15 +6,6 @@ import { App } from './app';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import ErrorPage from './error-page/error-page';
 
-/* const [species, setSpecies] = useState<SpeciesDTO[]>([]);
-
-async function rootLoader(inputValue: string) {
-  const data = await searchSpecies(inputValue);
-  const species = data.results;
-  setSpecies(species);
-  return { species };
-} */
-
 const router = createBrowserRouter([
   {
     path: '/',

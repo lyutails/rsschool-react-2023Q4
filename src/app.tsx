bd0b5079d7270c6525276321755641fd717eab95
lyutails <lyusya.cool@gmail.com>
2023-11-06 18:32:10 +0300

    feat: create browser router and provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,16 +1,21 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from 'react-router-dom';
 import { HeroPage } from './hero_page/hero_page';
 import ErrorPage from './error-page/error-page';
 import { CardDetails } from './card-details/card-details';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<HeroPage />} errorElement={<ErrorPage />}>
+      <Route path="species/:id" element={<CardDetails />}></Route>
+    </Route>
+  )
+);
+
 export function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HeroPage />} errorElement={<ErrorPage />}>
-          <Route path="species/:id" element={<CardDetails />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }

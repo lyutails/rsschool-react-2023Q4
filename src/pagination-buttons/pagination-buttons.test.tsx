a4f2eab90b51d38b5dd8f7bb40dfb1c3ba4b1802
lyutails <lyusya.cool@gmail.com>
2023-11-14 10:45:01 +0300

    feat: update vite config

---
+++ src/pagination-buttons/pagination-buttons.test.tsx
@@ -0,0 +1,8 @@
+/* import { render, screen } from '@testing-library/react';
+import { PaginationButtons } from './pagination-buttons';
+import { describe } from 'vitest'; */
+
+/* describe('PaginationButtons', () => {
+  render(<PaginationButtons />);
+  expect(screen.getByText('previous')).toBeI;
+}); */
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
